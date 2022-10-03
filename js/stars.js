b6f3daa7f2e84de1412c4fff5b814c3977341e96
maliszek97 <piotr.maliszewski97@gmail.com>
2022-10-03 20:11:00 +0200

    Change stars and techBG sections

--- js/stars.js
+++ js/stars.js
@@ -1,11 +1,12 @@
+const techSelenium = document.querySelectorAll('.technologies .tech-stars #selenium .star');
+const techJava = document.querySelectorAll('.technologies .tech-stars #java .star');
+const techJmeter = document.querySelectorAll('.technologies .tech-stars #java .star');
+const techPostman = document.querySelectorAll('.technologies .tech-stars #postman .star');
 const techCss = document.querySelectorAll('.technologies .tech-stars #css .star');
 const techHtml = document.querySelectorAll('.technologies .tech-stars #html .star');
 const techJs = document.querySelectorAll('.technologies .tech-stars #js .star');
-const techBootstrap = document.querySelectorAll('.technologies .tech-stars #bootstrap .star');
-const techRwd = document.querySelectorAll('.technologies .tech-stars #rwd .star');
-const techCPlus = document.querySelectorAll('.technologies .tech-stars #c-plus .star');
-const techGimp = document.querySelectorAll('.technologies .tech-stars #gimp .star');
-const techSeo = document.querySelectorAll('.technologies .tech-stars #seo .star');
+const techGit = document.querySelectorAll('.technologies .tech-stars #git .star');
+
 
 /* funkcja sleep do tworzenia przerwy */
 function sleep(ms) {
@@ -14,14 +15,15 @@ function sleep(ms) {
 
 /* liczba gwiazdek dla danej technologii */
 
+let seleniumNum = 3;
+let javaNum = 2;
+let jmeterNum = 2;
+let postmanNum = 2;
 let cssNum = 4;
 let htmlNum = 4;
 let jsNum = 2;
-let bootstrapNum = 3;
-let rwdNum = 4;
-let cPlusNum = 2;
-let gimpNum = 2;
-let seoNum = 2;
+let gitNum = 3;
+
 
 /*funkcje dodające gwiazki do każdej technologii równocześnie działające */
 
@@ -52,26 +54,26 @@ $(window).scroll(async function () {
 
         }
 
-        for (var i = 0; i < rwdNum; i++) {
-            techRwd[i].classList.add('active-star');
+        for (var i = 0; i < seleniumNum; i++) {
+            techSelenium[i].classList.add('active-star');
             await sleep(300);
 
         }
 
-        for (var i = 0; i < cPlusNum; i++) {
-            techCPlus[i].classList.add('active-star');
+        for (var i = 0; i < javaNum; i++) {
+            techJava[i].classList.add('active-star');
+            await sleep(300);
+
+        }
+
+        for (var i = 0; i < jmeterNum; i++) {
+            techJmeter[i].classList.add('active-star');
             await sleep(300);
 
         }
 
         for (var i = 0; i < gimpNum; i++) {
-            techGimp[i].classList.add('active-star');
-            await sleep(300);
-
-        }
-
-        for (var i = 0; i < seoNum; i++) {
-            techSeo[i].classList.add('active-star');
+            techGit[i].classList.add('active-star');
             await sleep(300);
         }
 
